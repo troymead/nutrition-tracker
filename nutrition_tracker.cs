ed83ec6fa11bcc16e044a52f16cb3060d1dd7d56
troymead <tspringmeadows@gmail.com>
2019-07-18 14:51:32 -0400

    test commit

--- nutrition_tracker.cs
+++ nutrition_tracker.cs
@@ -1,7 +1,11 @@
 // nutrition_tracker.cs
+//TODO:
+// need to create class/function to analyze difference in data set
+
+
 using System;
 class NutritionTracker {
     static void Main() {
-
+        int i = 5; // just a test
     }
 }
