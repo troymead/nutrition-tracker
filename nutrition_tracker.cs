c40aab7c9569c7f12383171294d00dd6260ad3e1
troymead <tspringmeadows@gmail.com>
2019-07-23 21:40:29 -0400

    added userData struct

--- nutrition_tracker.cs
+++ nutrition_tracker.cs
@@ -1,11 +1,24 @@
 // nutrition_tracker.cs
 //TODO:
-// need to create class/function to analyze difference in data set
+// need to create class/functions to analyze difference in data set(s)
+// still need to test
+
 
 
 using System;
+
+public struct UserData {
+    public string userName;
+    public double userWeight;
+    public double[] dailyCalories; // contains calorie count for a week (has seven entries)
+    public double[] weeklyCalories;
+    public UserData(string userName) {
+        this.userName = userName;
+    }
+}
 class NutritionTracker {
-    static void Main() {
-        int i = 5; // just a test
+    static void Main(string[] args) {
+        user1 = new UserData("troymead");
+        Console.WriteLine(user1.userName);
     }
 }
