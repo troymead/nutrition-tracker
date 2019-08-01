f9e41a436fceae72977acd3a25b1682c4022c992
troymead <tspringmeadows@gmail.com>
2019-08-01 13:11:33 -0400

    added printUserInfo function

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace nutrition_tracker
     {
         static void Main(string[] args)
         {
-            UserData user1 = new UserData("troymeadows", 20, "female", 125, 1.5, 2, 3);
-            Console.WriteLine(user1.userName); // test
-            Console.WriteLine("User's Age: {0}", user1.userAge); // test
+            UserData user1 = new UserData("troymeadows", 20, "female", 125, 65, 2, 3);
+            // Console.WriteLine(user1.userName); // test
+            // Console.WriteLine("User's Age: {0}", user1.userAge); // test
             int calorieMin = 50;
             int calorieMax = 10000;
             Random randNum = new Random();
@@ -17,10 +17,14 @@ namespace nutrition_tracker
                     user1.userCalories[i,j] = randNum.Next(calorieMin, calorieMax);
                 }
             }
-            Console.WriteLine("User Calories for Day 1: {0}", user1.userCalories[0,0]); // need to test
-
+            // Console.WriteLine("User Calories for Day 1: {0}", user1.userCalories[0,0]); // need to test
+            // double calGoal = 0;
+            // calGoal = userCalorieGoal(user1);
+            // Console.WriteLine("User 1's Daily Calorie Goal: {0}", calGoal);
+            printUserInfo(user1); // need to test
         }
-        public double userCalorieGoal(UserData user) {
+
+        static public double userCalorieGoal(UserData user) {
             double recCalories;
             recCalories = 0;
             if (user.userGender == "male") {
@@ -47,6 +51,32 @@ namespace nutrition_tracker
                     break;
             }
             return recCalories;
+        }
+
+        static public void printUserInfo(UserData user) {
+            Console.WriteLine();
+            Console.WriteLine("----- USER INFORMATION -----");
+            Console.WriteLine("User's Name: {0}", user.userName);
+            Console.WriteLine("User's Age: {0}", user.userAge);
+            Console.WriteLine("User Gender: {0}", user.userGender);
+            Console.WriteLine("User Weight: {0} lbs", user.userWeight);
+            Console.WriteLine("User Height: {0} in.", user.userHeight);
+            switch(user.weightStatus) {
+                case 1:
+                    Console.WriteLine("This user would like to lose weight.");
+                    break;
+                case 2:
+                    Console.WriteLine("This user would like to maintain their weight.");
+                    break;
+                case 3:
+                    Console.WriteLine("This user would like to gain weight.");
+                    break;
+                default:
+                    Console.WriteLine("The user did not enter a goal.");
+                    break;
+            }
+            Console.WriteLine("The user should consume {0} calories each day to work towards their goal.", userCalorieGoal(user));
+            Console.WriteLine();
         }
     }
 }
