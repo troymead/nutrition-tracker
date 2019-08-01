970bdfae141b3e31cbae23731403814fe4b21dd6
troymead <tspringmeadows@gmail.com>
2019-07-31 21:44:31 -0400

    add functin for calorie recommendation

--- Program.cs
+++ Program.cs
@@ -6,9 +6,47 @@ namespace nutrition_tracker
     {
         static void Main(string[] args)
         {
-            UserData user1 = new UserData("troymeadows", 20, 125, 1.5, 2);
+            UserData user1 = new UserData("troymeadows", 20, "female", 125, 1.5, 2, 3);
             Console.WriteLine(user1.userName); // test
             Console.WriteLine("User's Age: {0}", user1.userAge); // test
+            int calorieMin = 50;
+            int calorieMax = 10000;
+            Random randNum = new Random();
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 7; j++) {
+                    user1.userCalories[i,j] = randNum.Next(calorieMin, calorieMax);
+                }
+            }
+            Console.WriteLine("User Calories for Day 1: {0}", user1.userCalories[0,0]); // need to test
+
+        }
+        public double userCalorieGoal(UserData user) {
+            double recCalories;
+            recCalories = 0;
+            if (user.userGender == "male") {
+                recCalories = 66 + (6.3 * user.userWeight) + (12.9 * user.userHeight) - (6.8 * user.userAge);
+            }
+            else if (user.userGender == "female") {
+                recCalories = 655 + (4.3 * user.userWeight) + (4.7 * user.userHeight) - (4.7 * user.userAge);
+            }
+            switch (user.activity) {
+                case 1:
+                    recCalories = recCalories * 1.2;
+                    break;
+                case 2:
+                    recCalories = recCalories * 1.375;
+                    break;
+                case 3:
+                    recCalories = recCalories * 1.55;
+                    break;
+                case 4:
+                    recCalories = recCalories * 1.725;
+                    break;
+                case 5:
+                    recCalories = recCalories * 1.9;
+                    break;
+            }
+            return recCalories;
         }
     }
 }
--- nutrition_tracker.cs
+++ nutrition_tracker.cs
@@ -10,18 +10,23 @@ using System;
 
 public struct UserData {
     public string userName;
+    public string userGender;
     public int userAge;
-    public double userWeight, userHeight; // userHeight in meters
+    public double userWeight;
+    public double userHeight; // userHeight in inches
     // public double[] dailyCalories;
     // public double[] weeklyCalories; // contains calorie count for a week (has seven entries)
     public double[,] userCalories; // 2-d array; 4 weeks, 7 days in a week
     public int weightStatus; // 1 = Lose weight; 2 = Maintain weight; 3 = Gain Weight;
-    public UserData(string userName, int userAge, double userWeight, double userHeight, int weightStatus) {
+    public int activity; // 1 = sedentary, 2 = lightly active, 3 = moderately active, 4 = very active, 5 = extra active
+    public UserData(string userName, int userAge, string userGender, double userWeight, double userHeight, int weightStatus, int userActivity) {
         this.userName = userName;
         this.userAge = userAge;
+        this.userGender = userGender;
         this.userWeight = userWeight;
         this.userHeight = userHeight;
         this.weightStatus = weightStatus;
+        this.activity = userActivity;
         userCalories = new double[4,7];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 7; j++) {
