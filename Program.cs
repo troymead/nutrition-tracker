3d4ac86e19e8db71a63b8e3c344a32bacc6c86e1
meadowts <meadowts@mail.uc.edu>
2019-07-31 20:15:29 -0400

    fixed errors, added program file, minor changes

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nutrition_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserData user1 = new UserData("troymeadows", 20, 125, 1.5, 2);
+            Console.WriteLine(user1.userName); // test
+            Console.WriteLine("User's Age: {0}", user1.userAge); // test
+        }
+    }
+}
--- nutrition_tracker.cs
+++ nutrition_tracker.cs
@@ -11,36 +11,31 @@ using System;
 public struct UserData {
     public string userName;
     public int userAge;
-    public double userWeight;
-    public double userHeight; // in meters (to make it easier for calculations)
-    public double[] dailyCalories; // contains calorie count for a week (has seven entries)
-    public double[] weeklyCalories;
-
+    public double userWeight, userHeight; // userHeight in meters
+    // public double[] dailyCalories;
+    // public double[] weeklyCalories; // contains calorie count for a week (has seven entries)
+    public double[,] userCalories; // 2-d array; 4 weeks, 7 days in a week
     public int weightStatus; // 1 = Lose weight; 2 = Maintain weight; 3 = Gain Weight;
-    public UserData(string userName) {
-        this.userName = userName;
-    }
-
-    public UserData(string userName, int userAge) {
-        this.userName = userName;
-        this.userAge = userAge;
-    }
-
-    public UserData(string userName, int userAge, double userWeight, double userHeight) {
+    public UserData(string userName, int userAge, double userWeight, double userHeight, int weightStatus) {
         this.userName = userName;
         this.userAge = userAge;
         this.userWeight = userWeight;
         this.userHeight = userHeight;
+        this.weightStatus = weightStatus;
+        userCalories = new double[4,7];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 7; j++) {
+                userCalories[i,j] = 0;
+            }
+        }
+        
     }
 }
-class NutritionTracker {
-    static void Main(string[] args) {
-        user1 = new UserData("troymead");
-        user2 = new UserData("troymeadows", 20, 125, 1.65);
-        int minCalories = 500;
-        int maxCalories = 0; // placeholder; need to calculate for later
-        Console.WriteLine(user1.userName);
-        Console.WriteLine(user2.userAge);
+// class NutritionTracker {
+//     static void Main(string[] args) {
+//         user2 = new UserData("troymeadows", 20, 125, 1.65);
+//         int minCalories = 500;
+//         int maxCalories = 0; // placeholder; need to calculate for later
 
-    }
-}
+//     }
+// }
